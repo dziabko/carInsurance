cc9900a5bd448aeae203b8412f6b4625e2b3c30c
Daniel Ziabko <daniel399ziabko@gmail.com>
2018-10-01 13:16:31 -0400

    Added Policy state

---
+++ kotlin-source/src/main/kotlin/net/corda/examples/obligation/flows/InsureCarBaseFlow.kt
@@ -0,0 +1,40 @@
+package com.insureflight.flows
+
+import com.google.common.collect.ImmutableList
+import net.corda.core.contracts.StateAndRef
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.flows.FlowException
+import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.FlowSession
+import net.corda.core.flows.SignTransactionFlow
+import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.Party
+import net.corda.core.node.services.Vault
+import net.corda.core.node.services.queryBy
+import net.corda.core.node.services.vault.QueryCriteria
+import net.corda.core.transactions.SignedTransaction
+import com.insureflight.Policy
+
+/**
+ * An abstract FlowLogic class that is subclassed by the policy flows to
+ * provide helper methods and classes.
+ */
+abstract class InsureFlightBaseFlow : FlowLogic<SignedTransaction>() {
+
+    val firstNotary get() = serviceHub.networkMapCache.notaryIdentities.firstOrNull()
+            ?: throw FlowException("No available notary.")
+
+    fun getPolicyByLinearId(linearId: UniqueIdentifier): StateAndRef<Policy> {
+        val queryCriteria = QueryCriteria.LinearStateQueryCriteria(
+                null,
+                ImmutableList.of(linearId),
+                Vault.StateStatus.UNCONSUMED, null)
+
+        return serviceHub.vaultService.queryBy<Policy>(queryCriteria).states.singleOrNull()
+                ?: throw FlowException("Policy with id $linearId not found.")
+    }
+
+    fun resolveIdentity(abstractParty: AbstractParty): Party {
+        return serviceHub.identityService.requireWellKnownPartyFromAnonymous(abstractParty)
+    }
+}
---
+++ kotlin-source/src/main/kotlin/net/corda/examples/obligation/flows/IssuePolicy.kt
@@ -0,0 +1,4 @@
+package net.corda.examples.obligation.flows
+
+class IssuePolicy {
+}
