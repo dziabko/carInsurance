f4b3e00c5e2f891f9628b635d948fd9a44756d55
Daniel Ziabko <daniel399ziabko@gmail.com>
2018-10-01 13:17:22 -0400

    Added Policy & Driver States

--- kotlin-source/src/main/kotlin/net/corda/examples/obligation/Driver.kt
+++ kotlin-source/src/main/kotlin/net/corda/examples/obligation/Driver.kt
@@ -1,10 +1,19 @@
 package net.corda.examples.obligation
 
+import net.corda.core.contracts.Amount
+import net.corda.core.contracts.LinearState
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.Party
+import net.corda.core.utilities.toBase58String
+import java.util.*
 
 data class Driver(val wallet: Amount<Currency>,
                   val InsuranceCompany: AbstractParty,
                   val driver: AbstractParty,
+                  val paid: Amount<Currency>,
                   val coveragePercentage: Int,
+                  val policy: Policy,
                   override val linearId: UniqueIdentifier = UniqueIdentifier()) : LinearState {
 
     override val participants: List<AbstractParty> get() = listOf(InsuranceCompany, driver)
@@ -16,6 +25,6 @@ data class Driver(val wallet: Amount<Currency>,
     override fun toString(): String {
         val insurerString = (InsuranceCompany as? Party)?.name?.organisation ?: InsuranceCompany.owningKey.toBase58String()
         val driverString = (driver as? Party)?.name?.organisation ?: driver.owningKey.toBase58String()
-        return "Obligation($linearId): $driverString owes $insurerString $amount and has paid $paid so far."
+        return ""
     }
 }
